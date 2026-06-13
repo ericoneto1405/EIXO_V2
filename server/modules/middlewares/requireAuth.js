f96a021a30eb2e47031d2a4925f979f95e8f09c5
Erico <atacadosertao@gmail.com>
2026-06-12 22:52:32 -0300

    fix: limpar imports duplicados em requireAuth.js (usa constants.js)

--- server/modules/middlewares/requireAuth.js
+++ server/modules/middlewares/requireAuth.js
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { ALLOW_X_USER_ID, IS_PROD } from '../config/env.js';
+import { UUID_REGEX, allowXUserId } from '../config/constants.js';
 import { extractSessionTokenFromRequest, hashSessionToken, getSessionFromRequest } from './session.js';
 import { ensureSaasContextForUser, ensureFieldWorkerFarmAccess, isSaasContextError, BILLING_BLOCKED_STATES } from '../utils/saasContext.js';
 
 const prisma = new PrismaClient();
-const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-const allowXUserId = !IS_PROD && ALLOW_X_USER_ID;
 
 export const isFieldWorkerRequest = (req) => req.access?.appContext?.mode === 'field';
 
