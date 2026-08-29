6231ef1ba6011d0603ca9708d0abded302dc9ed1
Erico <atacadosertao@gmail.com>
2026-08-29 09:46:07 -0300

    fix: alinhamento do padding entre Rebanho e demais telas

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1192,7 +1192,7 @@ const AppContent: React.FC = () => {
                                 ref={contentScrollRef}
                                 className={
                                 activeView === 'Rebanho Comercial'
-                                        ? 'h-full overflow-x-hidden overflow-y-auto px-2 py-4 lg:px-3 lg:py-5'
+                                        ? 'h-full overflow-x-hidden overflow-y-auto px-4 py-4 lg:px-6 lg:py-5'
                                         : 'h-full overflow-x-hidden overflow-y-auto p-4 lg:p-6'
                             }>
                                 {hasNoFarms && activeView !== 'Fazendas' && activeView !== 'EIXO HQ' && activeView !== 'EIXO Mercado' ? <FirstFarmOnboarding /> : (
