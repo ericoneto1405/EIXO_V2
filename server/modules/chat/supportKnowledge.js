cba335daa4af79adaed2e3ab14ec155b7801aebc
Erico <atacadosertao@gmail.com>
2026-09-07 14:17:51 +0000

    docs(suporte): bump de revisão do conhecimento (date picker e onboarding)

--- server/modules/chat/supportKnowledge.js
+++ server/modules/chat/supportKnowledge.js
@@ -1,6 +1,6 @@
 import crypto from 'node:crypto';
 
-export const SUPPORT_KNOWLEDGE_REVISION = '2026-09-07.2';
+export const SUPPORT_KNOWLEDGE_REVISION = '2026-09-07.3';
 export const SUPPORT_KNOWLEDGE_UPDATED_AT = '2026-09-07';
 
 export const SUPPORT_TONE_RULES = [
