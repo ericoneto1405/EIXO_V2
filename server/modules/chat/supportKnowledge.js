74baeb079df96c1f1ec7b3673ff0cd689b4acb15
Erico <atacadosertao@gmail.com>
2026-09-08 01:40:40 +0000

    docs(suporte): bump de revisão do conhecimento (ajuste de permissões por módulo)

--- server/modules/chat/supportKnowledge.js
+++ server/modules/chat/supportKnowledge.js
@@ -1,7 +1,7 @@
 import crypto from 'node:crypto';
 
-export const SUPPORT_KNOWLEDGE_REVISION = '2026-09-07.3';
-export const SUPPORT_KNOWLEDGE_UPDATED_AT = '2026-09-07';
+export const SUPPORT_KNOWLEDGE_REVISION = '2026-09-08.1';
+export const SUPPORT_KNOWLEDGE_UPDATED_AT = '2026-09-08';
 
 export const SUPPORT_TONE_RULES = [
     'Seja cordial, solícito, positivo e direto.',
